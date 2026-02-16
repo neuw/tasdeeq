850ff01f5a54c7eaa38e7449caa0151f31f62f15
Karanbir Singh <krnbr@live.in>
2026-02-16 07:44:35 +0530

    formatting

--- core/src/main/java/com/neuwton/tasdeeq/models/DNSTasdeeqResult.java
+++ core/src/main/java/com/neuwton/tasdeeq/models/DNSTasdeeqResult.java
@@ -6,46 +6,46 @@ import java.util.List;
 import java.util.Map;
 
 public class DNSTasdeeqResult {
-        private final String domain;
-        private final Map<String, List<String>> records = new HashMap<>();
-        private final Map<String, String> errors = new HashMap<>();
-        private String connectionError = "no error";
+    private final String domain;
+    private final Map<String, List<String>> records = new HashMap<>();
+    private final Map<String, String> errors = new HashMap<>();
+    private String connectionError = "no error";
 
-        public DNSTasdeeqResult(String domain) {
-            this.domain = domain;
-        }
-
-        public void addRecords(String recordType, List<String> recordList) {
-            records.put(recordType, recordList);
-        }
-
-        public void addError(String recordType, String error) {
-            errors.put(recordType, error);
-        }
-
-        public void setConnectionError(String error) {
-            this.connectionError = error;
-        }
-
-        public String getDomain() {
-            return domain;
-        }
-
-        public List<String> getRecords(String recordType) {
-            return records.getOrDefault(recordType, new ArrayList<>());
-        }
-
-        public String getConnectionError() {
-            return connectionError;
-        }
-
-        @Override
-        public String toString() {
-            return "DNSQueryResult{" +
-                    "domain='" + domain + '\'' +
-                    ", records=" + records +
-                    ", errors=" + errors +
-                    ", connectionError='" + connectionError + '\'' +
-                    '}';
-        }
+    public DNSTasdeeqResult(String domain) {
+        this.domain = domain;
     }
+
+    public void addRecords(String recordType, List<String> recordList) {
+        records.put(recordType, recordList);
+    }
+
+    public void addError(String recordType, String error) {
+        errors.put(recordType, error);
+    }
+
+    public void setConnectionError(String error) {
+        this.connectionError = error;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public List<String> getRecords(String recordType) {
+        return records.getOrDefault(recordType, new ArrayList<>());
+    }
+
+    public String getConnectionError() {
+        return connectionError;
+    }
+
+    @Override
+    public String toString() {
+        return "DNSQueryResult{" +
+                "domain='" + domain + '\'' +
+                ", records=" + records +
+                ", errors=" + errors +
+                ", connectionError='" + connectionError + '\'' +
+                '}';
+    }
+}
